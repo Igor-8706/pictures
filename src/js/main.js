b7d75698789c5ee56f7fffe257d8ed03b01cf881
Igor <Igor-87@mail.ru>
2023-02-11 12:26:11 +0300

    added drag&drop

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ import pictureSize from "./modules/pictureSize";
 import accordeon from "./modules/accordeon";
 import burger from "./modules/burger";
 import scrolling from "./modules/scrolling";
+import drop from "./modules/drop";
 
 
 window.addEventListener('DOMContentLoaded', () =>{
@@ -30,7 +31,7 @@ pictureSize('.sizes-block');
 accordeon('.accordion-heading');
 burger('.burger-menu','.burger');
 scrolling('.pageup');
-
+drop();
 
 
 
